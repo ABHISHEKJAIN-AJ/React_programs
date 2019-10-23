e112472a3b84afbfc650ead514cd5e048ab1a2c9
ABHISHEKJAIN-AJ <42721122+ABHISHEKJAIN-AJ@users.noreply.github.com>
2019-10-23 11:26:53 +0100

    Create first_ninjas.js

---
+++ first_ninjas.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react';
+class Ninjas extends Component{
+    render() {
+        return (
+            <div className="ninja">
+                <div>name : abhi</div>
+                <div>age : 21</div>
+                <div>belt : black</div>
+
+            </div>
+        )
+    }
+
+}
+
+export default Ninjas;
